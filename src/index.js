b546fbf167692d3c6d4a9188d785006576c89f66
Joe Sak <joe@joesak.com>
2016-01-18 12:09:01 -0500

    Add a basic controlled input to SearchBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyB2S5pK20Hbo-Rj6Iy4fqmVGMRWSdngBHs';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return <div>
+    <SearchBar />
+  </div>
 }
 
 ReactDOM.render(
